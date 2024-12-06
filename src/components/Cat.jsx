630df60b16796b4bf326f3bf4a55d19aa6db99b9
Liubov Dav <lubasha0512@gmail.com>
2024-12-06 10:53:50 -0800

    Created first changes

---
+++ src/components/Cat.jsx
@@ -0,0 +1,11 @@
+const Cat = () => {
+    return(
+        <li>
+            <h3>Name of Cat</h3>
+            <p>🐈</p>
+            <button>Pet cat </button>
+        </li>
+    );
+};
+
+export default Cat;
---
+++ src/components/CatList.jsx
@@ -0,0 +1,19 @@
+import Cat from './Cat';
+
+
+const CatList = () => {
+    return (
+        <>
+            <h2>Cat List</h2>
+            <ul>
+                <Cat/>
+                <Cat/>
+                <Cat/>
+            </ul>
+
+        </>
+        
+    );
+};
+
+export default CatList;
